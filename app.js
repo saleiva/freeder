fcf9b6b1aa93dca35542e4fd356704f06843a29d
Javier Arce <javierarce@gmail.com>
2012-05-02 00:36:49 +0200

    Simplifies code

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ express = require('express')
 , http = require('http')
 , https = require('https')
 , fs = require('fs')
+, un = require('underscore')
 , everyauth = require('everyauth')
 , connect = require('connect')
 , everyauthRoot = __dirname + '/..'
@@ -68,6 +69,7 @@ ourl.readingList      = '/reader/api/0/stream/contents/user/-/state/com.google/r
 ourl.subscriptionList = '/reader/api/0/subscription/list?output=json';
 ourl.feedContents     = '/reader/api/0/stream/contents/';
 ourl.markAsRead       =  '/reader/api/0/edit-tag?client=sirope';
+ourl.markAsUnread     =  '/reader/api/0/edit-tag?client=sirope';
 ourl.addFeed          =  '/reader/api/0/subscription/edit';
 
 var app = module.exports = express.createServer(
@@ -111,250 +113,138 @@ app.configure('production', function() {
 });
 
 
-//var Sirope = (function() {
+var Sirope = (function() {
 
-//_getRequestOptions = function(host, port, path, method, headers) {
-
-//var options = {
-//host: host,
-//port: port,
-//path: path,
-//method: method,
-//headers: _.extend({ 'Content-Type': 'application/x-www-form-urlencoded' }, headers)
-//};
-
-//return options;
-//}
-
-//_getActionToken = function(req, res, callback) {
-//TODO: The actionToken lasts 30 min, so we shouldn't hit the url for every mark as read request
-//http://code.google.com/p/google-reader-api/wiki/ActionToken
-
-//if (req.session.refreshToken === false && req.session.actionToken !== null) return;
-
-//var headers = { 'Authorization': 'Bearer ' + req.session.accessToken };
-//var post_options = Sirope.getOptions(conf.HOST, conf.HTTPS_PORT, ourl.actionToken, "GET", headers);
-
-//var post_req = http.request(post_options, function(response) {
-
-//var actionToken = "";
-
-//reponse.on('data', function (chunk) {
-//actionToken += chunk;
-//});
-
-//reponse.on('end', function () {
-//req.session.refreshToken = false;
-//req.session.actionToken  = actionToken;
-
-//if (callback) {
-//callback(req, res);
-//}
-//});
-//});
-
-//post_req.end();
-
-//_addFeed = function() {
-
-//};
-
-//_markAsRead = function() {
-
-//};
-
-//_markAsUnRead = function() {
-
-//};
-
-//_request = function() {
-
-//};
-
-//return {
-//getActionToken: _getActionToken,
-//addFeed:        _addFeed,
-//markAsRead:     _markAsRead,
-//markAsUnRead:   _markAsUnRead,
-//request:        _request
-//}
-//}());
-
-function getActionToken(req, res, callback) {
-  // TODO: The actionToken lasts 30 min, so we shouldn't hit the url for every mark as read request
-  // http://code.google.com/p/google-reader-api/wiki/ActionToken
-
-  if (req.session.refreshToken === false && req.session.actionToken !== null) {
-
-    if (callback) {
-      callback(req, res);
-    }
-    return
+  // Generate a hash with request options
+  _getRequestOptions = function(host, port, path, method, headers) {
+    return { host: host, port: port, path: path, method: method, headers: headers };
   }
 
-  console.log("Ok, refreshing the token");
+  _get = function(res, accessToken, url) {
+    var options = { host: conf.HOST, port: conf.HTTP_PORT, path: url, method: 'GET', headers: { 'Authorization': 'Bearer ' + accessToken } };
 
-  var post_options = {
-    host: conf.HOST,
-    port: conf.HTTP_PORT,
-    path: ourl.actionToken,
-    method: 'GET',
-    headers: { 'Authorization': 'Bearer '+ req.session.accessToken }
-  };
+    http.get(options, function(resp) {
+      var data = "";
 
-  var post_req = http.request(post_options, function(resp) {
+      resp.on('data', function(chunk) {
+        data += chunk;
+      });
 
-    var actionToken = "";
-
-    resp.on('data', function (chunk) {
-      actionToken += chunk;
-    });
-
-    resp.on('end', function () {
-      console.log("Token refreshed! Look: ", actionToken);
-      req.session.refreshToken = false;
-      req.session.actionToken = actionToken;
-      callback(req, res);
-    });
-  });
-
-  post_req.end();
-}
-
-// Subscribes to feed
-function addFeed(req, res) {
-  var post_data = "s=feed/" + req.params.url + "&ac=subscribe&T=" + req.session.actionToken;
-
-  var post_options = {
-    host: conf.HOST,
-    port: conf.HTTPS_PORT,
-    path: ourl.addFeed,
-    method: 'POST',
-    headers: { 'Content-Type': 'application/x-www-form-urlencoded',
-      'Content-Length': post_data.length,
-      'Authorization': 'Bearer '+ req.session.accessToken }
-  };
-
-  var post_req = https.request(post_options, function(resp) {
-
-    data = "";
-
-    resp.on('data', function (chunk) {
-      data += chunk;
-    });
-
-    resp.on('end', function () {
-
-      if (data == "OK") { // the item has been marked as read
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
+      resp.on('end', function() {
+        res.writeHead(resp.statusCode, { 'Content-Type': 'application/json' });
         res.write(data);
         res.end();
-      }
+      });
+
+      resp.on('error', function(e) {
+        console.log(e);
+      });
+
     });
-  });
 
-  post_req.write(post_data);
-  post_req.end();
-}
-
-// Marks an item as read
-function markAsRead(req, res) {
-  console.log('Marking as read');
-
-  var post_data = "i=" + req.params.pid + "&a=user/-/state/com.google/read&a=user/-/state/com.google/tracking-kept-unread&s=" + req.params.url + "&T=" + req.session.actionToken;
-
-  var post_options = {
-    host: conf.HOST,
-    port: conf.HTTPS_PORT,
-    path: ourl.markAsRead,
-    method: 'POST',
-    headers: { 'Content-Type': 'application/x-www-form-urlencoded',
-      'Content-Length': post_data.length,
-      'Authorization': 'Bearer '+ req.session.accessToken }
   };
 
-  var post_req = https.request(post_options, function(resp) {
+  _post = function(res, options, data) {
+    var post_req = https.request(options, function(resp) {
 
-    var data = "";
+      var data = "";
 
-    resp.on('data', function (chunk) {
-      data += chunk;
-    });
+      resp.on('data', function (chunk) {
+        data += chunk;
+      });
 
-    resp.on('end', function () {
-      console.log("mark as read", resp.statusCode);
-
+      resp.on('end', function () {
         res.writeHead(resp.statusCode, { 'Content-Type': 'text/plain' });
         res.write(data);
         res.end();
-
+      });
     });
-  });
 
-  post_req.write(post_data);
-  post_req.end();
-}
-
-// Marks an item as unread
-function markAsUnread(req, res) {
-
-  var post_data = "i=" + req.params.pid + "&a=user/-/state/com.google/kept-unread&r=user/-/state/com.google/read&a=user/-/state/com.google/tracking-kept-unread&s=" + req.params.url + "&T=" + req.session.actionToken;
-
-  var post_options = {
-    host: conf.HOST,
-    port: conf.HTTPS_PORT,
-    path: ourl.markAsRead,
-    method: 'POST',
-    headers: { 'Content-Type': 'application/x-www-form-urlencoded',
-      'Content-Length': post_data.length,
-      'Authorization': 'Bearer '+ req.session.accessToken }
+    post_req.write(data);
+    post_req.end();
   };
 
-  var post_req = https.request(post_options, function(resp) {
+  _getActionToken = function(req, res, callback) {
 
-    var data = "";
+    if (req.session.refreshToken == false && req.session.actionToken != null) {
 
-    resp.on('data', function (chunk) {
-      data += chunk;
-    });
-
-    resp.on('end', function () {
-      if (data == "OK") { // the item has been marked as unread
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.write(data);
-        res.end();
+      if (callback) {
+        callback(req, res);
       }
+
+      return;
+    }
+
+    var
+      headers = { 'Authorization': 'Bearer ' + req.session.accessToken },
+      options = Sirope.getRequestOptions(conf.HOST, conf.HTTP_PORT, ourl.actionToken, "GET", headers);
+
+    var post_req = http.request(options, function(response) {
+
+      var actionToken = "";
+
+      response.on('data', function(chunk) {
+        actionToken += chunk;
+      });
+
+      response.on('error', function(e) {
+        console.log("Error", e);
+      });
+
+      response.on('end', function() {
+        console.log("Action token", actionToken);
+
+        req.session.refreshToken = false;
+        req.session.actionToken  = actionToken;
+
+        if (callback) {
+          callback(req, res);
+        }
+      });
     });
-  });
 
-  post_req.write(post_data);
-  post_req.end();
-}
+    post_req.end();
+  };
 
-// Makes a request with authorization headers
-function request(res, accessToken, url) {
+  _addFeed = function(req, res) {
+    var
+      data     = "s=feed/" + req.params.url + "&ac=subscribe&T=" + req.session.actionToken,
+      headers = { 'Content-Type': 'application/x-www-form-urlencoded', 'Content-Length': data.length, 'Authorization': 'Bearer ' + req.session.accessToken },
+      options  = Sirope.getRequestOptions(conf.HOST, conf.HTTPS_PORT, ourl.addFeed, "POST", headers);
 
-  var options = { host: conf.HOST, port: conf.HTTP_PORT, path: url, method: 'GET', headers: {'Authorization': 'Bearer '+ accessToken}};
+    Sirope.post(res, options, data);
+  };
 
-  http.get(options, function(resp) {
-    var data = "";
+  _markAsRead = function(req, res) {
+    var
+      data     = "i=" + req.params.pid + "&a=user/-/state/com.google/read&a=user/-/state/com.google/tracking-kept-unread&s=" + req.params.url + "&T=" + req.session.actionToken,
+      headers  = { 'Content-Type': 'application/x-www-form-urlencoded', 'Content-Length': data.length, 'Authorization': 'Bearer '+ req.session.accessToken },
+      options  = Sirope.getRequestOptions(conf.HOST, conf.HTTPS_PORT, ourl.markAsRead, "POST", headers);
 
-    resp.on('data', function (chunk) {
-      data +=chunk;
-    });
+    Sirope.post(res, options, data);
+  };
 
-    resp.on('end', function () {
-      console.log(resp.statusCode);
-      res.writeHead(resp.statusCode, { 'Content-Type': 'application/json' });
-      res.write(data);
-      res.end();
-    });
+  _markAsUnRead = function(req, res) {
+    var
+    data    = "i=" + req.params.pid + "&a=user/-/state/com.google/kept-unread&r=user/-/state/com.google/read&a=user/-/state/com.google/tracking-kept-unread&s=" + req.params.url + "&T=" + req.session.actionToken,
+    headers = { 'Content-Type': 'application/x-www-form-urlencoded', 'Content-Length': data.length, 'Authorization': 'Bearer '+ req.session.accessToken },
+    options = Sirope.getRequestOptions(conf.HOST, conf.HTTPS_PORT, ourl.markAsUnRead, "POST", headers);
 
-    resp.on('error', function(e) {
-      console.log(e);
-    });
-  });
-}
+    Sirope.post(res, options, data);
+  };
+
+  return {
+    getRequestOptions: _getRequestOptions,
+    getActionToken:    _getActionToken,
+    addFeed:           _addFeed,
+    markAsRead:        _markAsRead,
+    markAsUnRead:      _markAsUnRead,
+    get:               _get,
+    post:              _post
+  };
+
+}());
+
 
 // Routes
 app.get('/', function(req, res) {
@@ -380,42 +270,43 @@ app.get('/get/:query', function(req, res) {
     url = ourl.subscriptionList;
   }
 
-  request(res, req.session.accessToken, url);
+  Sirope.get(res, req.session.accessToken, url);
 });
 
 app.get('/get/feed/:url/:unread', function(req, res) {
   var url = null;
 
-  if (req.params.url == "all") {
-    url = ourl.readingList+"?&r=n&n=100";
+  if (req.params.url === "all") {
+    url = ourl.readingList + "?&r=n&n=100";
   } else {
-    url = ourl.feedContents+decodeURIComponent(req.params.url)+"?&r=n&n=100";
+    url = ourl.feedContents+decodeURIComponent(req.params.url) + "?&r=n&n=100";
   }
 
   if (req.params.unread === "t") {
     url += "&xt=user/-/state/com.google/read";
   }
 
-  request(res, req.session.accessToken, url);
+  Sirope.get(res, req.session.accessToken, url);
 });
 
+
 /**
- * ROUTES
- */
+* ROUTES
+*/
 
 // Subscribe to feed service
 app.get('/subscribe/:url', function(req, res) {
-  getActionToken(req, res, addFeed);
+  Sirope.getActionToken(req, res, Sirope.addFeed);
 });
 
 // Mark as read service
 app.get('/markasread/:url/:pid', function(req, res) {
-  getActionToken(req, res, markAsRead);
+  Sirope.getActionToken(req, res, Sirope.markAsRead);
 });
 
 // Mark as unread service
 app.get('/markasunread/:url/:pid', function(req, res) {
-  getActionToken(req, res, markAsUnread);
+  Sirope.getActionToken(req, res, Sirope.markAsUnread);
 });
 
 app.get('/refresh', function(req, res) {
@@ -423,11 +314,13 @@ app.get('/refresh', function(req, res) {
 
   if (req.session && req.session.userName) {
 
-    getActionToken(req, res, function() {
+    Sirope.getActionToken(req, res, function() {
       console.log("Refreshed. Rendering", req.session.userName);
+
       req.session.refreshToken = true;
       res.redirect('/' + req.session.userName);
     });
+
   } else {
     res.redirect("/auth/google");
   }
